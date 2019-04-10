ec85e1cef318e1d5ba35f780c38a271a383bce30
raitchait <49256373+raitchait@users.noreply.github.com>
2019-04-10 18:29:25 -0400

    Add files via upload

---
+++ Calculator/oop_homework/src/telecommunucation/Obstract.java
@@ -0,0 +1,18 @@
+package telecommunucation;
+
+public class Obstract {
+
+	public static void main(String[] args) {
+		//abstract parent class
+
+		abstract class Call {
+		   //abstract method
+
+		}
+		
+		
+		}
+
+	}
+
+
